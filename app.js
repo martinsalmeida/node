6c887888e5ef0f33f747a03b26dc405c8f8a38b0
Daniel Augusto <daniel.arquiteto@gmail.com>
2020-01-12 23:08:03 -0300

    configuração da conexão com banco de dados

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-//Routas
+//Rotas
 const router = express.Router();
 router.get("/", (req, res) => {
   res.send("Ola Mundo");
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.set("port", 8080);
+dotenv.config({ path: "variables.env" });
+
+//Conexão com o Banco de dados
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+mongoose.connection.on("error", error => {
+  console.error("ERRO: " + error.message);
+});
+
+app.set("port", process.env.PORT || 7000);
 const server = app.listen(app.get("port"), () => {
   console.log("servidor rodando na porta" + server.address().port);
 });
