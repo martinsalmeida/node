9500efdb6c64e2db4714aa601182504a48110e68
Daniel Augusto <daniel.arquiteto@gmail.com>
2020-01-13 20:37:42 -0300

    Instalando a view engine

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mustache = require("mustache-express");
 const router = require("./routes/index");
 
 //Configuraçôes
@@ -6,5 +7,9 @@ const app = express();
 app.use("/", router);
 
 app.use(express.json());
+
+app.engine("mst", mustache());
+app.set("view engine", "mst");
+app.set("views", __dirname + "/views");
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Ola Mundo");
-
-  //GET: req.query
-  //POST: req.body
-  //PARAMETROS: req.params
-
-  //SEND: ENVIAR TEXTOS
-  //JSON: ENVIAR OBJETOS JSON
-});
-
-router.get("/sobre", (req, res) => {
-  res.send("Pagina Sobre");
-});
-
-router.get("/posts/:id", (req, res) => {
-  res.send("id do post: " + req.params.id);
+  res.render("home", {
+    nome: "Daniel",
+    idade: 30
+  });
 });
 
 module.exports = router;
