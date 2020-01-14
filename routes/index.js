ec055a4c8a3275c1c83bfdca7b73cdb050fa1b49
Daniel Augusto <daniel.arquiteto@gmail.com>
2020-01-13 21:31:42 -0300

    Configurando mustache e partials

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("home", {
+  let obj = {
     nome: "Daniel",
-    idade: 30
-  });
+    idade: 30,
+    mostrar: false,
+    ingredientes: [
+      { nome: "arroz", qt: 20 },
+      { nome: "feijao", qt: 30 }
+    ],
+    interesses: ["node", "js", "react"],
+    teste: []
+  };
+  res.render("home", obj);
 });
 
 module.exports = router;
